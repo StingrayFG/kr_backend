653124d55266d26eedd3109c3b83a5f1af5ad356
zami62 <112836383+zami62@users.noreply.github.com>
2023-05-19 10:04:56 +0300

    renamed adapters

---
+++ Adapters/OrdersAdapter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dal_proj.DAL;
+using kr_lib;
+
+#nullable enable
+
+namespace bll_proj.Adapters
+{
+    public class OrdersAdapter
+    {
+        private OrdersDAL DAL = new OrdersDAL();
+
+        public void AddOrder(Order order)
+        {
+            DAL.AddOrder(order);
+        }
+
+        public int GetLastID()
+        {
+            return DAL.GetLastID();
+        }
+
+        public List<Order>? GetOrders()
+        {
+            return DAL.GetOrders();
+        }
+
+        public Order? GetOrderByID(int orderID)
+        {
+            return DAL.GetOrderByID(orderID);
+        }
+
+        public void UpdateOrderCustomer(int orderID, int newCustomerID)
+        {
+            DAL.UpdateOrderCustomer(orderID, newCustomerID);
+        }
+
+        public void UpdateOrderPaymentMethod(int orderID, string newPaymentMethod)
+        {
+            DAL.UpdateOrderPaymentMethod(orderID, newPaymentMethod);
+        }
+
+        public void RemoveOrder(int orderID)
+        {
+            DAL.RemoveOrder(orderID);
+        }
+    }
+}
---
+++ Adapters/PartsAdapter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dal_proj.DAL;
+using kr_lib;
+
+#nullable enable
+
+namespace bll_proj.Adapters
+{
+    public class PartsAdapter
+    {
+        private PartsDAL DAL = new PartsDAL();
+
+        public void AddPart(Part part)
+        {
+            DAL.AddPart(part);
+        }
+
+        public int GetLastID()
+        {
+            return DAL.GetLastID();
+        }
+
+        public List<Part>? GetParts()
+        {
+            return DAL.GetParts();
+        }
+
+        public Part? GetPartByID(int partID)
+        {
+            return DAL.GetPartByID(partID);
+        }
+
+        public List<Part>? GetPartsByName(string name)
+        {
+            return DAL.GetPartsByName(name);
+        }
+
+        public void UpdatePartName(int partID, string newName)
+        {
+            DAL.UpdatePartName(partID, newName);
+        }
+
+        public void RemovePart(int partID)
+        {
+            DAL.RemovePart(partID);
+        }
+    }
+}
